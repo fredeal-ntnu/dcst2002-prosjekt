ed3bdd38145eea357a905c788319c7d300cf44bb
Simon Leirvik Zahl <simonlz@stud.ntnu.no>
2023-10-28 17:51:15 +0200

    latest render

--- client/src/services/question_service.tsx
+++ client/src/services/question_service.tsx
@@ -2,48 +2,169 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:3000/api/v2';
 
-export type Question = {
-  question_id: number;
-  title: string;
-  text: string;
-  view_count: number;
-  confirmed_answer: boolean;
+export type Recipe = {
+  oppskrift_id: number;
+  oppskrift_navn: string;
+  oppskrift_beskrivelse: string;
+  oppskrift_steg: string;
+  ant_pors: number;
+  bilde_adr: string;
+  kategori_id: number;
+  land_id: number;
+  ant_like: number;
+  liked: boolean;
 };
 
-class TaskService {
-  /**
-   * Get task with given id.
-   */
-  get(id: number) {
-    return axios.get<Question>('/questions/' + id).then((response) => response.data);
-  }
+export type Recipe_Content = {
+  oppskrift_id: number;
+  ingred_id: number;
+  mengde: string;
+  maleenhet: string;
+};
 
+export type Country = {
+  land_id: number;
+  land_navn: string;
+};
+export type Category = {
+  kategori_id: number;
+  kategori_navn: string;
+};
+export type IngredientToShoppinglist = {
+  ingred_id: number;
+  mengde: number;
+  maleenhet: string;
+};
+export type ElementShoppingList = {
+  ingred_id: number;
+  ingred_navn: string;
+  mengde: number;
+  maleenhet: string;
+};
+export type Ingredient = {
+  ingred_id: number;
+  ingred_navn: string;
+};
+export type List = {
+  id: number;
+  ingred_id: number;
+  mengde: string;
+  maleenhet: string;
+};
+export type IceboxIngredient = {
+  ingred_id: number;
+  ingred_navn: string;
+};
+class Service {
   /**
    * Get all tasks.
    */
-  getAll() {
-    return axios.get<Question[]>('/questions').then((response) => response.data);
+  getAllRepice() {
+    return axios.get<Recipe[]>('/').then((response) => response.data);
   }
 
-  /**
-   * Create new task having the given title.
-   *
-   * Resolves the newly created task id.
-   */
-  create(title: string, description: string) {
+  getRecipe(id: number) {
+    return axios.get<Recipe[]>('/recipe/' + id).then((response) => response.data);
+  }
+  getRecipeContent(id: number) {
+    return axios.get<Recipe_Content[]>('/recipecontent/' + id).then((response) => response.data);
+  }
+  getAllCountry() {
+    return axios.get<Country[]>('/country').then((response) => response.data);
+  }
+  getAllCategory() {
+    return axios.get<Category[]>('/category').then((response) => response.data);
+  }
+  getAllIngredient() {
+    return axios.get<Ingredient[]>('/ingredient').then((response) => response.data);
+  }
+  getShoppingList() {
+    return axios.get<List[]>('/shoppinglist').then((response) => response.data);
+  }
+  /* addItemToShoppingList(item: ElementShoppingList) {
+    return axios.post<{}>('/additemshoppinglist', {item: item}).then((response) => response.data);
+  } */
+  addIngredient(ingredient: List) {
     return axios
-      .post<{ id: number }>('/tasks', { title: title, description: description})
+      .post<{}>('/addingredient', { ingredient: ingredient })
+      .then((response) => response.data);
+  }
+  getAllIceboxIngredients() {
+    return axios.get<IceboxIngredient[]>('/icebox').then((response) => response.data);
+  }
+  getAllRecipeContent() {
+    return axios.get<Recipe_Content[]>('/recipecontent').then((response) => response.data);
+  }
+  deleteIceboxIngredient(ingred_id: number) {
+    return axios
+      .delete<IceboxIngredient>('/deleteiceboxingredient/' + ingred_id)
+      .then((response) => response.data);
+  }
+  createIngredient(name: string) {
+    return axios.post<{}>('/newingredient', { name: name }).then((response) => response.data);
+  }
+
+  createCountry(name: string) {
+    return axios.post<{}>('/newcountry', { name: name }).then((response) => response.data);
+  }
+  createCategory(name: string) {
+    return axios.post<{}>('/newcategory', { name: name }).then((response) => response.data);
+  }
+
+  createRecipe(recipe: Recipe) {
+    return axios
+      .post<{ id: number }>('/createrecipe', { recipe: recipe })
       .then((response) => response.data.id);
   }
 
-  delete(id: number) {
-    return axios.delete('/tasks/' + id).then((response) => response.data)
+  addIngredientToIcebox(selectedIceboxIngredient: IceboxIngredient) {
+    return axios
+      .post<{}>('/addingredienttoicebox', { selectedIceboxIngredient: selectedIceboxIngredient })
+      .then((response) => response.data);
   }
 
-  update(task: Task) {
-    return axios.put('/tasks',task).then((response) => response.data)
+  createRecipeIngredient(recipe_content: Recipe_Content[]) {
+    return axios
+      .post<Recipe_Content>('/create_recipe_ingredient', { recipe_content: recipe_content })
+      .then((response) => response.data);
+  }
+  updateRecipeIngredient(recipeContent: Recipe_Content[]) {
+    return axios
+      .put('/update_recipe_ingredient', { recipeContent: recipeContent })
+      .then((response) => response.data);
+  }
+  updateIngredientShoppingList(ingredient: List) {
+    return axios
+      .put<{}>('/updateingredient', { ingredient: ingredient })
+      .then((response) => response.data);
+  }
+  deleteIngredientShoppingList(id: number) {
+    return axios
+      .delete<{}>('/deleteingredientshoppinglist/' + id)
+      .then((response) => response.data);
+  }
+  deleteAllShoppingList() {
+    return axios.delete<{}>('/deleteallshoppinglist').then((response) => response.data);
+  }
+  updateRecipe(recipe: Recipe) {
+    return axios
+      .put('/update_recipe/' + recipe.oppskrift_id, { recipe: recipe })
+      .then((response) => response.data);
+  }
+  deleteIngredient(recipe_id: number, ingred_id: number) {
+    return axios
+      .delete<Recipe_Content>('/deleteingredient/' + recipe_id + '/' + ingred_id)
+      .then((response) => response.data);
+  }
+  deleteRecipe(id: number) {
+    return axios.delete<Recipe_Content>('/deleterecipe/' + id).then((response) => response.data);
+  }
+  likeRecipe(oppskrift_id: number, liked: boolean) {
+    return axios
+      .put<{}>('/recipelike/' + oppskrift_id, { liked: liked })
+      .then((response) => response.data);
   }
 }
 
-const taskService = new TaskService();
-export default taskService;
+const service = new Service();
+export default service;
