a5b6c6a0d9825e0a7b6a6358a157526a2b6e9c8f
Bjørne Morgan Øvreås <bjorne@dhcp-10-22-10-235.wlan.ntnu.no>
2023-11-05 15:25:58 +0100

    deleted

--- client/src/components/index-signed.tsx
+++ client/src/components/index-signed.tsx
@@ -1,27 +1,2 @@
-import * as React from 'react';
-import { Component } from 'react-simplified';
-import { NavLink } from 'react-router-dom';
-import {NavBar, Card, Alert, CardQuestions, CardHome, Row, Column } from '../widgets';
-import service, { Question } from '../service';
-export class IndexSigned extends Component {
-  questions: Question[] = [];
-  render() {
-    return (
-      <Card title="">
-        <div className="row">
-          <CardHome header="Profile"
-            items={[{ label: "Questions", to: "/questions" }, { label: "Tags", to: "/tags" }, {label: "New Question", to: "createquestion"}, {label: "My Questions", to: "myquestion"}]}/>
-          <CardQuestions header="Top Questions">
-          {this.questions.map((question) => (
-            <Row key={question.question_id}>
-              {question.title}<br></br>{question.text}
-            </Row>
-          ))}
-        </CardQuestions>
-          
-        </div> 
-      </Card>
-    );
-  }};
 
  
