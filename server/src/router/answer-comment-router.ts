83ab8c54682d8a433310fda7cd2d1149ed51fde8
Simon Leirvik Zahl <simonlz@stud.ntnu.no>
2023-11-13 11:50:27 +0100

    ferdig med services og routes

--- server/src/router/answer-comment-router.ts
+++ server/src/router/answer-comment-router.ts
@@ -3,18 +3,58 @@ import {answerCommentService} from '../service/answer-comment_services';
 
 const answerCommentRouter = express.Router();
 
-//Get all comments by question id
 
-answerCommentRouter.get
+// Get all answer comments by answer id
+answerCommentRouter.get('/questions/:questionId/answers/:answerId/comments', (request, response) => {
+    const answerId = Number(request.params.answerId);
+    answerCommentService
+    .getAnswerCommentByAnswerId(answerId)
+    .then((rows) => response.send(rows))
+    .catch((error) => response.status(500).send(error));
+});
 
+//create new answer comment
+answerCommentRouter.post('/questions/:questionId/answers/:answerId/comments', (request, response) => {
+    const data = request.body;
+    if (
+        typeof data.text == 'string' &&
+        data.text.length != 0
+    )
+        answerCommentService
+        .createAnswerComment(data.text, data.answer_id)
+        .then((id) => response.send({ id: id}))
+        .catch((error) => response.status(500).send(error));
+    else response.status(400).send('Missing answer comment router 400');
+});
 
+// Delete answer comment
+answerCommentRouter.delete('/questions/:questionId/answers/:answerId/comments/:commentId', (request, response) => {
+    const commentId = Number(request.params.commentId);
+    answerCommentService
+    .deleteAnswerComment(commentId)
+    .then((rows) => response.send(rows))
+    .catch((error) => response.status(500).send(error));
+});
 
-answerCommentRouter.post
-
-
+// Update answer comment
+answerCommentRouter.put('/questions/:questionId/answers/:answerId/comments/:commentId', (request, response) => {
+    const data = request.body;
+    if (
+        typeof data.text == 'string' &&
+        data.text.length != 0
+    )
+        answerCommentService
+        .updateAnswerComment(data.text, Number(data.answer_comment_id))
+        .then((id) => response.send({ id: id}))
+        .catch((error) => response.status(500).send(error));
+    else response.status(400).send('Missing answer comment router 400');
+});
 
 
 export default answerCommentRouter;
+
+
+
 
 
 
--- server/src/service/question-comment_services.ts
+++ server/src/service/question-comment_services.ts
@@ -1,4 +1,3 @@
-
 import pool from '../mysql-pool';
 import type { RowDataPacket, ResultSetHeader } from 'mysql2';
 
@@ -9,36 +8,71 @@ export type Question_Comment_Content = {
 };
 
 class Service {
+  //get answer comment by question id
+  getQuestionCommentByQuestionId(question_id: number) {
+    return new Promise<Question_Comment_Content[]>((resolve, reject) => {
+      pool.query(
+        'SELECT * FROM question_comments WHERE question_id = ?',
+        [question_id],
+        (error, results: RowDataPacket[]) => {
+          if (error) return reject(error);
 
-    //getAnswerComment by answer id
-    getQuestionCommentByQuestionId() {
-    
-    }
+          resolve(results as Question_Comment_Content[]);
+        },
+      );
+    });
+  }
 
-    
+  createQuestionComment(text: string, question_id: number) {
+    return new Promise<number>((resolve, reject) => {
+      pool.query(
+        'INSERT INTO question_comments SET text=?, question_id=?',
+        [text, question_id],
+        (error, results: ResultSetHeader) => {
+          if (error) return reject(error);
 
+          resolve(results.insertId);
+        },
+      );
+    });
+  }
 
-    createQuestionComment {
+  /**
+   * Delete answer comment with answer id.
+   */
 
-    }
+  deleteQuestionComment(question_comment_id: number) {
+    return new Promise<void>((resolve, reject) => {
+      pool.query(
+        'DELETE FROM question_comments WHERE question_comment_id = ?',
+        [question_comment_id],
+        (error, results: ResultSetHeader) => {
+          if (error) return reject(error);
+          if (results.affectedRows == 0) return reject(new Error('No row deleted'));
 
-    /**
-     * Delete answer comment with answer id.
-     */
+          resolve();
+        },
+      );
+    });
+  }
 
-    deleteQuestionComment(answer_id: number) {
-           
-    }
+  /**
+   * Update answer Comment with given id.
+   */
 
-    /**
-     * Update answer Comment with given id.
-     */
-    
-    updateQuestionComment{
+  updateQuestionComment(text: string, question_comment_id: number) {
+    return new Promise<void>((resolve, reject) => {
+      pool.query(
+        'UPDATE question_comments SET text=? WHERE question_comment_id=?',
+        [text, question_comment_id],
+        (error, _results) => {
+          if (error) return reject(error);
 
-    }
-    
+          resolve();
+        },
+      );
+    });
+  }
 }
 
-
-export const questionCommentService = new Service(); 
+export const questionCommentService = new Service();
