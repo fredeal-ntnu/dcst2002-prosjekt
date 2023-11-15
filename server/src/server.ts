127dc146491f2e49a0ec978d56f0b4b3e5b30009
Bjørne Morgan Øvreås <bjorne@dhcp-10-22-11-155.wlan.ntnu.no>
2023-11-15 10:46:03 +0100

    changes

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,7 +10,9 @@ import { config } from 'dotenv';
 // Serve client files
 app.use(express.static(path.join(__dirname, '/../../client/public')));
 
-app.get('/api/v2/current_user')
+app.get('/api/v2/current_user', (req, res) => {
+  res.send(req.user);
+});
 
 const port = 3000;
 app.listen(port, () => {
