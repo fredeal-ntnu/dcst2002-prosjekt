36e5f8384108cb210705f37ef7c152d3fe33d773
bjorneov <bmorganovreas@gmail.com>
2023-11-19 13:47:53 +0100

    test av test

--- client/test/all-questions.test.tsx
+++ client/test/all-questions.test.tsx
@@ -1,22 +1,20 @@
-// Import necessary libraries and components
 import React from 'react';
-import { shallow, mount } from 'enzyme';
-import { AllQuestions } from 'src/components/all-question';
-import service from 'src/service';
+import { shallow } from 'enzyme';
+import { AllQuestions } from 'src/components/all-question'; // Adjust the import path as needed
+import service from 'src/service'; // Adjust the import path as needed
 
-// Mock the service module to simulate API calls
-jest.mock('src/service', () => ({
-  getAllQuestions: jest.fn(() => Promise.resolve([])),
-  getTopFiveQuestions: jest.fn(() => Promise.resolve([])),
-  getUnansweredQuestions: jest.fn(() => Promise.resolve([])),
-  getAllTags: jest.fn(() => Promise.resolve([])),
-}));
+jest.mock('src/service'); // Mock the service module
 
 describe('AllQuestions Component', () => {
   let wrapper;
 
   beforeEach(() => {
-    // Shallow render the component before each test
+    // Mock the service methods
+    service.getAllQuestions.mockResolvedValue([]);
+    service.getTopFiveQuestions.mockResolvedValue([]);
+    service.getUnansweredQuestions.mockResolvedValue([]);
+    service.getAllTags.mockResolvedValue([]);
+
     wrapper = shallow(<AllQuestions />);
   });
 
@@ -24,60 +22,89 @@ describe('AllQuestions Component', () => {
     expect(wrapper.exists()).toBe(true);
   });
 
-  it('initializes state correctly', () => {
-    expect(wrapper.state('questions')).toEqual([]);
-    expect(wrapper.state('filter')).toBe('all');
-    expect(wrapper.state('tags')).toEqual([]);
-    expect(wrapper.state('search')).toBe('');
+  describe('Rendering Child Components', () => {
+    it('renders SideMenu', () => {
+      expect(wrapper.find('SideMenu').exists()).toBe(true);
+    });
+
+    it('renders QuestionCard for each question', () => {
+      wrapper.setState({ questions: [{ id: 1 }, { id: 2 }] });
+      expect(wrapper.find('QuestionCard').length).toBe(2);
+    });
+
+    it('renders SideMenu with correct props', () => {
+      expect(wrapper.find('SideMenu').prop('header')).toEqual('Menu');
+      // Add assertions for other props as needed
+    });
   });
 
-  it('loads questions on mount', () => {
-    // Mock the componentDidMount lifecycle method
-    const loadQuestionsMock = jest.spyOn(AllQuestions.prototype, 'loadQuestions');
-    
-    // Mount the component to trigger the lifecycle method
-    mount(<AllQuestions />);
-    
-    // Expect the loadQuestions method to be called once
-    expect(loadQuestionsMock).toHaveBeenCalledTimes(1);
+  describe('State Management', () => {
+    it('updates state correctly on filter change', () => {
+      wrapper.find('Form.Select').simulate('change', { target: { value: 'popular' } });
+      expect(wrapper.state('filter')).toBe('popular');
+    });
 
-    // Restore the mock after the test
-    loadQuestionsMock.mockRestore();
+    it('updates state correctly on search input change', () => {
+      wrapper.find('Form.Input[type="text"]').simulate('change', { currentTarget: { value: 'test search' } });
+      expect(wrapper.state('search')).toBe('test search');
+    });
   });
 
-  it('handles filter change correctly', () => {
-    const select = wrapper.find('Form.Select');
-    
-    // Simulate a filter change event
-    select.simulate('change', { target: { value: 'popular' } });
-    
-    // Expect the component state to be updated
-    expect(wrapper.state('filter')).toBe('popular');
+  describe('Component Methods', () => {
+    it('handleFilterChange updates filter state and calls loadQuestions', () => {
+      const instance = wrapper.instance();
+      jest.spyOn(instance, 'loadQuestions');
+      instance.handleFilterChange({ target: { value: 'unanswered' } });
+      expect(wrapper.state('filter')).toEqual('unanswered');
+      expect(instance.loadQuestions).toHaveBeenCalled();
+    });
+
+    describe('loadQuestions Method', () => {
+      it('calls correct service method based on filter state', async () => {
+        await wrapper.instance().loadQuestions();
+        expect(service.getAllQuestions).toHaveBeenCalled();
+
+        wrapper.setState({ filter: 'popular' });
+        await wrapper.instance().loadQuestions();
+        expect(service.getTopFiveQuestions).toHaveBeenCalled();
+
+        wrapper.setState({ filter: 'unanswered' });
+        await wrapper.instance().loadQuestions();
+        expect(service.getUnansweredQuestions).toHaveBeenCalled();
+      });
+    });
   });
 
-  it('renders questions correctly', async () => {
-    const questions = [
-      { id: 1, title: 'Question 1' },
-      { id: 2, title: 'Question 2' },
-    ];
+  describe('Lifecycle Methods', () => {
+    it('calls loadQuestions and getAllTags on component mount', () => {
+      const loadQuestionsSpy = jest.spyOn(AllQuestions.prototype, 'loadQuestions');
+      const getAllTagsSpy = jest.spyOn(service, 'getAllTags');
 
-    // Mock the service method to return questions
-    service.getAllQuestions.mockResolvedValueOnce(questions);
-
-    // Update the component to reflect the new state
-    await wrapper.update();
-
-    // Expect the QuestionCard components to be rendered based on the number of questions
-    expect(wrapper.find('QuestionCard')).toHaveLength(questions.length);
+      shallow(<AllQuestions />);
+      expect(loadQuestionsSpy).toHaveBeenCalled();
+      expect(getAllTagsSpy).toHaveBeenCalled();
+    });
   });
 
-  it('handles search input correctly', () => {
-    const input = wrapper.find('Form.Input');
+  describe('Conditional Rendering and Behavior', () => {
+    it('displays the correct number of questions', () => {
+      const testQuestions = [{ id: 1 }, { id: 2 }, { id: 3 }];
+      wrapper.setState({ questions: testQuestions });
+      const displayedText = wrapper.find('Column').at(0).text();
+      expect(displayedText).toContain(`${testQuestions.length} questions in total`);
+    });
 
-    // Simulate a search input change event
-    input.simulate('change', { currentTarget: { value: 'search query' } });
-
-    // Expect the component state to be updated
-    expect(wrapper.state('search')).toBe('search query');
+    it('filters questions based on search input', () => {
+      const testQuestions = [
+        { id: 1, title: 'React Testing' },
+        { id: 2, title: 'Enzyme Tutorial' },
+        { id: 3, title: 'Jest Overview' }
+      ];
+      wrapper.setState({ questions: testQuestions, search: 'enzyme' });
+      const filteredQuestions = wrapper.instance().questions.filter(q => q.title.toLowerCase().includes('enzyme'));
+      expect(filteredQuestions.length).toBe(1);
+    });
   });
+
+  // Additional tests (if any) go here
 });
