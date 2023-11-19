a4530d96d547d3480e76a2934997017e69be0d42
bjorneov <bmorganovreas@gmail.com>
2023-11-19 15:59:08 +0100

    all-qs test start

---
+++ client/test/widgets.tst.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { AllQuestions } from 'src/components/all-question'; // Adjust the import path as needed
+import { Column } from 'src/widgets';
+import service from 'src/service'; // Adjust the import path as needed
+
+jest.mock('src/service'); // Mock the service module
+
+describe('Collum widget test', () => {
+  test('Draws a column without properties', () => {
+    const wrapper = shallow(<Column>Test</Column>);
+
+    expect(
+      wrapper.containsMatchingElement(
+        <div className="col">
+        <div className="float-start">
+          Test
+        </div>
+      </div>
+      )
+    ).toEqual(true);
+  });
+})
+
+describe('Collum widget test', () => {
+  test('Draws a column with width', () => {
+    const wrapper = shallow(<Column width={3}>Test</Column>);
+
+    expect(
+      wrapper.containsMatchingElement(
+        <div className="col-3">
+        <div className="float-start">
+          Test
+        </div>
+      </div>
+      )
+    ).toEqual(true);
+  });
+})
+
+describe('Collum widget test', () => {
+  test('Draws a column with width and right', () => {
+    const wrapper = shallow(<Column width={3} right>Test</Column>);
+
+    expect(
+      wrapper.containsMatchingElement(
+        <div className="col-3">
+        <div className="float-end">
+          Test
+        </div>
+      </div>
+      )
+    ).toEqual(true);
+  });
+})
+
+
+
+  
