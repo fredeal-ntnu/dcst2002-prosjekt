a2a4daf0d9be5c9b89aa2244971826529aad9063
Simon Leirvik Zahl <simonlz@stud.ntnu.no>
2023-11-20 14:46:00 +0100

    henter variablene fra .env

--- server/src/router/login_router.ts
+++ server/src/router/login_router.ts
@@ -4,6 +4,12 @@ import express from "express";
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import {User, userService} from "../service/user_services";
+/**
+  * Passport.js authentication middleware for Google OAuth2.
+ * - `GOOGLE_Client_ID`
+ * - `GOOGLE_Client_Secret`
+ * - `callbackURL`
+ */
 
 
 const loginRouter = express.Router();
@@ -30,17 +36,17 @@ passport.deserializeUser(function (user: User, cb) {
 passport.use(
   new GoogleStrategy(
     {
-      clientID: '847026350985-ski1ogfb6klvrfjfrjqkhiapg02219js.apps.googleusercontent.com',
-      clientSecret:  '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',
-      callbackURL: 'http://localhost:3000/api/v1/auth/google/callback',
+      clientID: process.env.GOOGLE_Client_ID || '',
+      clientSecret: process.env.GOOGLE_Client_Secret || '',
+      callbackURL: process.env.callbackURL || '',
     },
-    function (accessToken, refreshToken, profile, cb) {
+    function (accessToken: string, refreshToken: string, profile: any, cb: any) {
       userService.findOrCreate({
         google_id: profile.id,
         username: profile.displayName,
         email: profile._json.email || '',
       }, cb);
-      }
+    }
   )
 );
 
